8f350d2638385cb15859e02158ad5ded10adb452
Céline <celine.inthaaphay@stagiairesmns.fr>
2024-11-24 16:03:18 +0100

    🐛fix messages not sending while testing angular application

--- src/main/java/com/rentals/responses/MessageResponse.java
+++ src/main/java/com/rentals/responses/MessageResponse.java
@@ -1,24 +1,10 @@
 package com.rentals.responses;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class MessageResponse {
-    private Integer id;
     private String message;
-    private String created_at;
-    private String updated_at;
-    private Integer user_id;
-    private Integer rental_id;
-
-    public MessageResponse(Integer id, String message, String created_at, String updated_at, Integer user_id, Integer rental_id) {
-        this.id = id;
-        this.message = message;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-        this.user_id = user_id;
-        this.rental_id = rental_id;
-    }
 }
